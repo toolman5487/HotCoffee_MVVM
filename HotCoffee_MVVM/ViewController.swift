6a1c2e33f6850fe0457fee4fca6ea9d41699f93b
Willy <willy548798@gmail.com>
2025-03-03 17:13:29 +0800

    Initial Commit

---
+++ HotCoffee_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotCoffee_MVVM
+//
+//  Created by Willy Hsu on 2025/3/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
