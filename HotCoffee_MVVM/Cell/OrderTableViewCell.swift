a110bf98df5061673895d90f920f9593ba759b58
Willy <willy548798@gmail.com>
2025-03-05 22:36:02 +0800

    完成點餐系統 儲存餐點 POST 並 GET 餐點到主頁

---
+++ HotCoffee_MVVM/Cell/OrderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderTableViewCell.swift
+//  HotCoffee_MVVM
+//
+//  Created by Willy Hsu on 2025/3/5.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
